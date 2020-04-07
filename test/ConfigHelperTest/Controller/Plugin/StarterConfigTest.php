e81a209b1e6f1670ce7656a01051f77584c1eba4
Alex <tasmaniski@gmail.com>
2020-04-07 07:48:22 +0200

    fix unit test for ZF3

--- test/ConfigHelperTest/Controller/Plugin/StarterConfigTest.php
+++ test/ConfigHelperTest/Controller/Plugin/StarterConfigTest.php
@@ -10,14 +10,14 @@ class StarterConfigTestTest extends \PHPUnit_Framework_TestCase
     {
         $serviceManager      = Bootstrap::getServiceManager();
         $configHelperFactory = new \ConfigHelper\Factory\Controller\Plugin\StarterConfigFactory();
-        $configHelper        = $configHelperFactory->createService($serviceManager->get('controllerpluginmanager'));
+        $configHelper        = $configHelperFactory->__invoke($serviceManager, '', []);
 
         $this->assertInstanceOf('ConfigHelper\Controller\Plugin\StarterConfig', $configHelper);
     }
 
     public function testInstance()
     {
-        $testConfig   = array('key_in_config' => 'value');
+        $testConfig   = ['key_in_config' => 'value'];
         $configHelper = new \ConfigHelper\Controller\Plugin\StarterConfig($testConfig);
 
         $this->assertInstanceOf('Zend\Config\Config', $configHelper());
@@ -25,7 +25,7 @@ class StarterConfigTestTest extends \PHPUnit_Framework_TestCase
 
     public function testGetKeyFromConfig()
     {
-        $testConfig   = array('key_in_config' => 'value');
+        $testConfig   = ['key_in_config' => 'value'];
         $configHelper = new \ConfigHelper\Controller\Plugin\StarterConfig($testConfig);
 
         $this->assertEquals($testConfig['key_in_config'], $configHelper('key_in_config'));
